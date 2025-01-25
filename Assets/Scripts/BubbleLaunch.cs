e3e888628fe8cb10b3a947414ceb4a077747f0db
Colin <colingschulte@gmail.com>
2025-01-25 00:23:31 -0600

    Basic Controls - End of Day 1

---
+++ Assets/Scripts/BubbleLaunch.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BubbleLaunch : MonoBehaviour
+{
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= transform.right * Time.deltaTime * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,16 @@ public class PlayerMovement : MonoBehaviour
     public Collider2D collider;
     public Collider2D ground;
     public Collider2D platform;
+    public Collider2D platform2;
+    public Collider2D platform3;
     public float speed;
     public float jumpForce;
     float jumpPower;
+    public float movementLR;
+    public BubbleLaunch bubble;
+    public Transform shotPosition;
+    public SpriteRenderer sprite;
+    public bool jumpPressed;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +29,55 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float movementLR = Input.GetAxis ("Horizontal");
-        if (Input.GetButtonDown("Jump") && (collider.bounds.Intersects(ground.bounds) || collider.bounds.Intersects(platform.bounds)))
+        movementLR = Input.GetAxis ("Horizontal");
+        if (Input.GetButtonDown("Jump") && (collider.bounds.Intersects(ground.bounds) || ((collider.bounds.Intersects(platform.bounds) || collider.bounds.Intersects(platform2.bounds) || collider.bounds.Intersects(platform3.bounds)) && Input.GetAxisRaw("Vertical") >= 0)))
+        {
+            jumpPressed = true;
+        }
+        else
+        {
+            if (jumpPower > 0)
+            {
+                //jumpPower -= 1f;
+            }
+        }
+
+        
+
+        //firing bubbles
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bubble, shotPosition.position, shotPosition.rotation);
+        }
+
+        if (Input.inputString != "")
+        {
+            Debug.Log(Input.inputString);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(movementLR > 0 && !sprite.flipX)
+        {
+            //shotPosition.transform.rotation = 180f;
+            Vector3 rot = shotPosition.rotation.eulerAngles;
+            rot = new Vector3(rot.x, rot.y + 180, rot.z);
+            shotPosition.rotation = Quaternion.Euler(rot);
+            sprite.flipX = true;
+        }
+        else if (movementLR < 0 && sprite.flipX)
+        {
+            Vector3 rot = shotPosition.rotation.eulerAngles;
+            rot = new Vector3(rot.x, rot.y + 180, rot.z);
+            shotPosition.rotation = Quaternion.Euler(rot);
+            sprite.flipX = false;
+        }
+
+        if (jumpPressed)
         {
             jumpPower = jumpForce;
+            jumpPressed = false;
         }
         else
         {
@@ -34,7 +86,6 @@ public class PlayerMovement : MonoBehaviour
                 jumpPower -= 1f;
             }
         }
-
-        rbody.velocity = new Vector2 (movementLR * speed, jumpPower);
+        rbody.velocity = new Vector2(movementLR * speed, jumpPower);
     }
 }
